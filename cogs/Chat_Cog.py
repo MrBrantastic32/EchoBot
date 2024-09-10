a1b2e000f50d7a0929aaca05a5419bb36f312184
unknown <Brandon.bpw12@gmail.com>
2024-09-10 18:34:01 +0100

    Minor Change V.0.2.5.1
    -> Changed the ai bot so that the text does not end halfway through a sentence and now completes a full sentence / sentences.
    
    18:33 - 10/09/2024

--- cogs/Chat_Cog.py
+++ cogs/Chat_Cog.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 from transformers import pipeline
-import tensorflow as tf
 import re
 
 ###-Creating the Chat_Cog-###
@@ -9,7 +8,7 @@ import re
 class ChatCog(commands.Cog):  
     def __init__(self, bot):
         self.bot = bot
-        self.generator = pipeline('text-generation', model='gpt2', pad_token_id = 50256, eos_token_id = 50256)  
+        self.generator = pipeline('text-generation', model='gpt2', pad_token_id=50256, eos_token_id=50256)  
 
 ###-Truncating sentence-###
 
@@ -19,36 +18,31 @@ class ChatCog(commands.Cog):
             return text[:len(text) - match.end()]
         return text
 
-
-
     @discord.app_commands.command(name="chat", description="Generate responses from AI")
     async def generate(self, interaction: discord.Interaction, prompt: str):
         await interaction.response.defer()
-        print("Deffered response, generating responses...")
+        print("Deferred response, generating responses...")
         try:
             response = self.generator(prompt, max_length=100, min_length=1, num_return_sequences=1, 
-                          no_repeat_ngram_size=2, top_p=0.9, temperature=1, early_stopping=True)
+                                      no_repeat_ngram_size=2, top_p=0.9, temperature=1, early_stopping=True)
             generated_text = response[0]['generated_text']
              
-
 ###-Post Processing the text-###
             generated_text = self.truncate_to_sentence(generated_text)
             print("Generated text:", generated_text)
 
-
 ###-Creating an Embed-###
 
-            embed = discord.Embed(title = "Echo Response", 
-                                  description = generated_text, 
-                                  color = discord.Color.purple())
+            embed = discord.Embed(title="Echo Response", 
+                                  description=generated_text, 
+                                  color=discord.Color.purple())
             embed.set_author(name=f"{interaction.user.display_name}: {prompt}",
-                              icon_url=interaction.user.display_avatar.url)
+                             icon_url=interaction.user.display_avatar.url)
             
             await interaction.followup.send(embed=embed) 
         except Exception as e:
             print("Error generating text:", e)  
             await interaction.followup.send("Sorry, something went wrong while generating the response.")  
-
 
 async def setup(bot):
     print('cog added')
